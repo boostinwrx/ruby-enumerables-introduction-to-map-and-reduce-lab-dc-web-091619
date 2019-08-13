2d034f3cc52fbbff94222322e12368d701fa3764
ANTONIO J ABRAHAM <tony.calablava@gmail.com>
2019-08-13 22:11:36 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -42,6 +42,6 @@ def reduce_to_total(source_array, starting_point)
   counter = 0
   new_array = [ ]
   while counter < source_array.length do
-    source_array[]
+    source_array[counter]
     counter += 1
   end
