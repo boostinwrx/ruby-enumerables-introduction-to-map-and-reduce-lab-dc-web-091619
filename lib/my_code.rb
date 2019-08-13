ea1a3a77df607cd70bcad0e272f065de4aed97c6
ANTONIO J ABRAHAM <tony.calablava@gmail.com>
2019-08-13 22:01:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,5 +1,11 @@
 source_array = [1,2,3,4]
 
 def map_to_negativize(source_array)
-  source_array.slice()
+   counter = 0
+  new_array = [ ]
+  while counter < array.length do
+    new_array.push(array[counter]*-1)
+    counter += 1
+  end
+p new_array
 end
