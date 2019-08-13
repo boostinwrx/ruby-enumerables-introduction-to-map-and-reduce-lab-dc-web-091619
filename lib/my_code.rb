423fdca74f42abb74df49a6fdbe5aed260aeb426
ANTONIO J ABRAHAM <tony.calablava@gmail.com>
2019-08-13 22:06:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -18,3 +18,13 @@ def map_to_no_change(source_array)
   end
 return new_array
 end 
+
+def map_to_double(source_array)
+   counter = 0
+  new_array = [ ]
+  while counter < source_array.length do
+    new_array.push(source_array[counter]*2)
+    counter += 1
+  end
+return new_array
+end 
