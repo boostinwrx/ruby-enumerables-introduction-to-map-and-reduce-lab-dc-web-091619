7c349c6efe103ea6428bf9f0052bc63cccd10979
ANTONIO J ABRAHAM <tony.calablava@gmail.com>
2019-08-13 22:08:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -39,4 +39,9 @@ def map_to_square(source_array)
 return new_array
 end 
 def reduce_to_total(source_array, starting_point)
-  
+  counter = 0
+  new_array = [ ]
+  while counter < source_array.length do
+    new_array.push(source_array[counter]**2)
+    counter += 1
+  end
