7dba168fbf912cb2a987b8297cddadc76f6e031f
ANTONIO J ABRAHAM <tony.calablava@gmail.com>
2019-08-13 22:16:26 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -42,6 +42,8 @@ def reduce_to_total(source_array, starting_point)
   counter = 0
   reduced = 0
   while counter < source_array.length do
-    source_array[counter].to_s ++
+    source_array[counter].to_s + reduced = reduced
     counter += 1
   end
+puts reduced
+end
